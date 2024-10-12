ae77e42fbf907aa4db27aa308d09ca3232b28fcc
Your Name <you@example.com>
2024-10-12 10:33:39 +0200

    testing

---
+++ probackend/main.tf
@@ -0,0 +1,88 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.0.1"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "rg-probackend-tfstatesaad"  
+    storage_account_name = "sabetfssaadpro"                      
+    container_name       = "tfstatesaadpro"                       
+    key                  = "probackend.terraform.tfstate"      
+  }
+
+
+}
+
+provider "azurerm" {
+  subscription_id = "efc1e7b1-5729-4eea-b33e-12cc6b1c0183"
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+    key_vault {
+      purge_soft_delete_on_destroy    = true
+      recover_soft_deleted_key_vaults = true
+    }
+  }
+}
+
+resource "azurerm_resource_group" "rg_backend" {
+  name     = var.rg_backend_name
+  location = var.rg_backend_location
+}
+
+resource "azurerm_storage_account" "sa_backend" {
+  name                     = var.sa_backend
+  resource_group_name      = azurerm_resource_group.rg_backend.name
+  location                 = azurerm_resource_group.rg_backend.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+}
+
+resource "azurerm_storage_container" "sc_backend" {
+  name = var.sc_backend
+  storage_account_name = azurerm_storage_account.sa_backend.name
+  container_access_type = "private"
+}
+
+
+data "azurerm_client_config" "current" {}
+
+
+resource "azurerm_key_vault" "kv_backend" {
+  name                        = var.kv_backend_name
+  location                    = azurerm_resource_group.rg_backend.location
+  resource_group_name         = azurerm_resource_group.rg_backend.name
+  enabled_for_disk_encryption = true
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = false
+
+  sku_name = "standard"
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+
+    key_permissions = [
+      "Get","List","Create",
+    ]
+
+    secret_permissions = [
+      "Get","Set","List",
+    ]
+
+    storage_permissions = [
+      "Get","Set","List",
+    ]
+  }
+}
+
+resource "azurerm_key_vault_secret" "sa_backend_accesskey" {
+  name         = var.sa_backend_accesskey_name
+  value        = azurerm_storage_account.sa_backend.primary_access_key
+  key_vault_id = azurerm_key_vault.kv_backend.id
+}
